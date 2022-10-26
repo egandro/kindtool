9e1fd1ccf503506ec23cdfd9800571a1ca55d7c2
Harald Fielker <harald.fielker@gmail.com>
2022-10-26 21:38:20 +0200

    .

--- src/kindtool/cli.py
+++ src/kindtool/cli.py
@@ -1,38 +1,60 @@
 import argparse
+from email.policy import default
 
 from kindtool import __app_name__, __version__, cmdinit, cmdup, cmddestroy, templates
 
-# # Create the parser
-# parser = argparse.ArgumentParser(__app_name__)
+def add_default_arguments(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument( '--directory', '-d', type=str,
+        metavar='DIR',
+        help="directory of Kindfile (default is current working directory)", required=False)
 
-# # Add an argument
-# parser.add_argument('--name', type=str, required=False)
-
-
-def main() -> None:
+def main_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(prog=__app_name__)
     parser.add_argument('--version', action='version',
                     version='%(prog)s {version}'.format(version=__version__))
+    return parser
 
+def create_parser_init(parent: argparse.ArgumentParser) -> None:
+    name = 'init'
+    help = 'create a new Kindfile'
+
+    parser = parent.add_parser(name, help=help)
+    parser.add_argument( '--directory', '-d', type=str,
+        metavar='DIR',
+        help="destination directory (default is current working directory)", required=False)
+
+def create_parser_up(parent: argparse.ArgumentParser) -> None:
+    name = 'up'
+    help = 'start a cluster'
+
+    parser = parent.add_parser(name, help=help)
+    add_default_arguments(parser)
+
+def create_parser_destroy(parent: argparse.ArgumentParser) -> None:
+    name = 'destroy'
+    help = 'stops a cluster'
+
+    parser = parent.add_parser(name, help=help)
+    add_default_arguments(parser)
+    parser.add_argument('--force','-f', action='store_true',
+        help="force the deletion")
+
+
+def main() -> None:
+
+    parser = main_parser()
     subparser = parser.add_subparsers(dest='command')
-
-    login = subparser.add_parser('login', help='verbose output')
-    login.add_argument('--username', type=str, required=True)
-    login.add_argument('--password', type=str, required=True)
-
-    register = subparser.add_parser('register')
-    register.add_argument('--firstname', type=str, required=True)
-    register.add_argument('--lastname', type=str, required=True)
-    register.add_argument('--username', type=str, required=True)
-    register.add_argument('--email', type=str, required=True)
-    register.add_argument('--password', type=str, required=True)
+    create_parser_init(subparser)
+    create_parser_up(subparser)
+    create_parser_destroy(subparser)
 
     args = parser.parse_args()
-    if args.command == 'login':
-        print('Logging in with username:', args.username,'and password:', args.password)
-    elif args.command == 'register':
-        print('Creating username', args.username,'for new member', args.firstname, args.lastname,
-            'with email:', args.email,
-            'and password:', args.password)
+
+    if args.command == 'init':
+        print(f'init with {args.directory}')
+    elif args.command == 'up':
+        print(f'up with {args.directory}')
+    elif args.command == 'destroy':
+        print(f'destroy with {args.directory}, {args.force}')
     else:
         parser.print_usage()
